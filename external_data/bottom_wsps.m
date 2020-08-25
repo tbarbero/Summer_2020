c7fe2f420d6269f40bce851871d9fc7208cd0146
Tyler <tbarbero@ucsd.edu>
2020-08-24 22:46:19 -0700

    cleaning up script

--- external_data/bottom_wsps.m
+++ external_data/bottom_wsps.m
@@ -1,18 +1,22 @@
 % Script that plots a site's windspeed data using same subset of days **
 % some days may be misisng due to bad data or instrument error.
+
 clear
 clc
 
 % read in bottom days
 load('bottomdays.mat');
 
-% tbl = readMESOWEST('CQ121.2020-08-07.csv'); % hourly
-tbl = readMESOWEST('KTRM.2020-08-07.csv'); % y
-% tbl = readMESOWEST('KNJK.2020-08-10.csv'); % hourly % kinda portrays southerly flow -- sitll mnts flow too (SW)
-% tbl = readMESOWEST('KIPL.2020-08-10.csv'); % y
-% tbl = readMESOWEST('E3298.2020-08-10.csv'); y
-% tbl = readMESOWEST('D3583.2020-08-10.csv');%----bad
-% tbl = readMESOWEST('MXCB1.2020-08-10.csv'); y
+% main sites 
+% tbl = readMESOWEST('KTRM.2020-08-07.csv'); % valley site
+% tbl = readMESOWEST('KIPL.2020-08-10.csv'); % plain site
+% tbl = readMESOWEST('E3298.2020-08-10.csv'); valley site
+% tbl = readMESOWEST('MXCB1.2020-08-10.csv'); plain site
+
+% other sites
+% tbl = readMESOWEST('D3583.2020-08-10.csv');
+% tbl = readMESOWEST('KNJK.2020-08-10.csv');
+% tbl = readMESOWEST('CQ121.2020-08-07.csv');
 
 % get metadata for site
 tmp = tbl.stationID{4};
@@ -63,10 +67,3 @@ subplot(1,3,3);plot(x,wdirs);grid;xticks([0:2:24]);yticks([0:15:360]);add_degs;x
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0.1, 0.32, 0.7, 0.55])
 fig = strcat(site,'.png');
 % saveas(gcf,fig)
-%% notes
-% KIPL site looks a little worse when using same subset of days. --
-% -- KIPL reoslution not that great, could cause bad data
-% KNJK bad data (hourly)
-
-% might not align PERFECTLY NW-SE valley flow b/c some bottom 50% westerly winds still
-% could be in dataset and would dilute the NW-SE valley flow
