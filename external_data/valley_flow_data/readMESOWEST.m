7168d32941936b5784ea7c7b1724a583838d8697
Tyler <tbarbero@ucsd.edu>
2020-08-25 01:32:22 -0700

    changes to file

--- external_data/valley_flow_data/readMESOWEST.m
+++ external_data/valley_flow_data/readMESOWEST.m
@@ -25,36 +25,5 @@ opts = setvaropts(opts, 2, "InputFormat", 'yyyy-MM-dd''T''HH:mm:ss''Z');
 % opts.ExtraColumnsRule = "ignore";
 % opts.EmptyLineRule = "read";
 
-% Import the data
 out = readtable(filename, opts);
-
-% generate separate date, time variables
-% time = NaN(numel(out.datetime));
-% date = NaN(numel(out.datetime));
-
-% for i=1:numel(out.date_time,1)
-%     if ~isnan(out.date_time(i))
-%         tmp = out.date_time(i);
-%         j = find(tmp=='T');
-%         date = 
-% u = NaN(numel(out.windstring),1);
-% v = NaN(numel(out.windstring),1);
-% for i = 1:numel(out.windstring)
-%     if ~isnan(out.start_hour(i))
-%         tmp = out.windstring{i}; % dir/wspd 360/0.52
-%         j = find(tmp=='/');
-%         dir = str2double(tmp(1:j-1));
-%         U = str2double(tmp(j+1:end));
-%         if ~isnan(U) && isnan(dir); dir = 0; end
-%         dir = 270 - dir; % convert to "math" wind direction
-%         dir(dir<0) = dir(dir<0) + 360;
-%         u(i) = U*cosd(dir);
-%         v(i) = U*sind(dir); 
-%         U(i) = U;
-%     end
-% end
-% out.u = u;
-% out.v = v;
-
-
 end
