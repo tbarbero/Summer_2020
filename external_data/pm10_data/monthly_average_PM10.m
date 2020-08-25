8304f14ba483c4e126f810e444def15af7db6cee
Tyler <tbarbero@ucsd.edu>
2020-08-25 01:25:37 -0700

    clean up pm10 script

--- external_data/pm10_data/monthly_average_PM10.m
+++ external_data/pm10_data/monthly_average_PM10.m
@@ -1,50 +1,49 @@
-% Plot diurnal PM10 data
-% function PM10(mon,dy)
-tbl = readPM10('PM10_SHR_2020.csv');
+% create average pm10 plot as a function of hour
+
+clear
+clc
+
+tbl = readPM10('PM10_SHR_2020.csv'); % hourly data in PST time
 load('dustydays.mat');
 
 % clean data
-tbl(2683:2694,:) = [];
+tbl(2683:end,:) = [];
 g = find(tbl.value==0);
-tbl.value(g) = NaN;
+tbl.value(g) = NaN; 
 
-% retrieve data 
-[y,m,d] = datevec(tbl.date);
-time = datetime(y,m,d,tbl.start_hour,0,0); % (PST time)
+% create variables (time, pm10)
+[y,m,d] = ymd(tbl.date);
+time = datetime(y,m,d,tbl.start_hour,0,0,'TimeZone','America/Los_Angeles');
+dates = datetime(year(time),month(time),day(time),'TimeZone','America/Los_Angeles');
 PM10 = tbl.value;
-
-% create temporary time var
-fakedates = datetime(y,m,d,'InputFormat','dd:mm:yyyy');
 clearvars y m d
 
-% exclude data from dusty days
-g = ~ismember(fakedates,dustydays);
-PM10 = PM10(g);
+% exclude dusty days
+g = ~ismember(dates,dustydays);
 time = time(g);
+PM10 = PM10(g);
 
-% constrain data to before mid-may
+% constrain data prior to may 21
 g = month(time)<5 | month(time)==5 & day(time)<21;
-PM10 = PM10(g);
 time = time(g);
+PM10 = PM10(g);
 
-%
-% create numerical times from datetimes
-numtime = hour(time(g))+minute(time(g))/60;
+% averages as a function of hour over all data
+numtime = hour(time);
 
 x = 0:23;
 
 for i=1:numel(x)
     g = find(numtime==x(i));
     
-    % get hourly averages for PM10 over specified month
-    A = PM10(g);
-    B = A(~isnan(A));
-    PM10avgs(i) = mean(B);
+    tmp = PM10(g);
+    tmp = tmp(~isnan(tmp)); % remove nans 
+    PM10avgs(i) = mean(tmp);
 end
-clearvars i A B g
+clearvars i tmp g
 
 % plot
 plot(x,PM10avgs);grid on
 xlabel('Hour (PST)');ylabel('PM10 (ug/m^3)');
-title('Monthly Averaged PM10');xlim([0,23]);xticks([0:2:23])
-legend('PM10','Location','NW')
+title('PM10 averaged as a function of hour');xlim([0,23]);xticks([0:2:23])
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.32, 0.6, 0.55])
